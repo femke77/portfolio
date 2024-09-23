de70224a8747c2c62a2d02e49eaf6f986218bfd7
femke77 <megan.meyers.388@gmail.com>
2024-09-23 01:23:32 -0700

    really working code this time, basic starter with colored boxes.

--- src/components/home/IconTicker.jsx
+++ src/components/home/IconTicker.jsx
@@ -1,72 +1,121 @@
 import React, { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 
-export default function IconTicker() {
-  const tickerRef = useRef(null);
+const colors = ["#f38630", "#6fb936", "#ccc", "blue"];
+
+export default function HorizontalLoop() {
+  const boxesRef = useRef([]);
+  const wrapperRef = useRef(null);
 
   useEffect(() => {
-    const ul = tickerRef.current;
-
-    // Wrap all <li> elements in a <span> ticker-wrapper
-    const tickerWrapper = document.createElement("span");
-    tickerWrapper.classList.add("ticker-wrapper");
-
-    // Move items into tickerWrapper
-    Array.from(ul.children).forEach((item) => {
-      tickerWrapper.appendChild(item);
+    const boxes = gsap.utils.toArray(boxesRef.current);
+    gsap.set(boxes, {
+      backgroundColor: gsap.utils.wrap(colors),
     });
 
-    // Clear existing children from the <ul> before appending
-    ul.innerHTML = '';
-    ul.appendChild(tickerWrapper);
-
-    // Clone the tickerWrapper for infinite scrolling
-    const tickerClone = tickerWrapper.cloneNode(true);
-    ul.appendChild(tickerClone);
-
-    const totalWidth = tickerWrapper.offsetWidth;
-    const tickerWidth = ul.offsetWidth;
-
-    // Set initial position of ticker-wrapper
-    gsap.set(tickerWrapper, { x: tickerWidth });
-
-    // Create the infinite scroll animation
-    const tl = gsap.timeline({ repeat: -1 });
-
-    tl.to(tickerWrapper, {
-      duration: totalWidth / 50, // Adjust speed here
-      x: `-${totalWidth}`,
-      ease: "none",
-      onComplete: () => {
-        gsap.set(tickerWrapper, { x: tickerWidth });
-      },
-    });
+    const loop = horizontalLoop(boxes, { paused: false, repeat: -1 });
 
     return () => {
-      // Cleanup on unmount
-      tl.kill();
+      loop.kill(); // Cleanup on unmount
     };
   }, []);
 
+  function horizontalLoop(items, config) {
+    items = gsap.utils.toArray(items);
+    config = config || {};
+    let tl = gsap.timeline({
+      repeat: config.repeat,
+      paused: config.paused,
+      defaults: { ease: "none" },
+      onReverseComplete: () => tl.totalTime(tl.rawTime() + tl.duration() * 100),
+    });
+
+    const length = items.length;
+    const startX = items[0].offsetLeft;
+    const times = [];
+    const widths = [];
+    const xPercents = [];
+    let curIndex = 0;
+    const pixelsPerSecond = (config.speed || 1) * 100;
+
+    gsap.set(items, {
+      xPercent: (i, el) => {
+        const w = (widths[i] = parseFloat(gsap.getProperty(el, "width", "px")));
+        xPercents[i] = parseFloat(gsap.getProperty(el, "x", "px")) / w * 100;
+        return xPercents[i];
+      },
+    });
+
+    gsap.set(items, { x: 0 });
+
+    const totalWidth =
+      items[length - 1].offsetLeft +
+      (xPercents[length - 1] / 100) * widths[length - 1] -
+      startX +
+      items[length - 1].offsetWidth * gsap.getProperty(items[length - 1], "scaleX");
+
+    for (let i = 0; i < length; i++) {
+      const item = items[i];
+      const curX = (xPercents[i] / 100) * widths[i];
+      const distanceToStart = item.offsetLeft + curX - startX;
+      const distanceToLoop = distanceToStart + widths[i] * gsap.getProperty(item, "scaleX");
+
+      tl.to(item, {
+        xPercent: (curX - distanceToLoop) / widths[i] * 100,
+        duration: distanceToLoop / pixelsPerSecond,
+      }, 0)
+        .fromTo(item, {
+          xPercent: (curX - distanceToLoop + totalWidth) / widths[i] * 100,
+        }, {
+          xPercent: xPercents[i],
+          duration: (curX - distanceToLoop + totalWidth - curX) / pixelsPerSecond,
+          immediateRender: false,
+        }, distanceToLoop / pixelsPerSecond)
+        .add("label" + i, distanceToStart / pixelsPerSecond);
+
+      times[i] = distanceToStart / pixelsPerSecond;
+    }
+
+    function toIndex(index, vars) {
+        vars = vars || {};
+        if (Math.abs(index - curIndex) > length / 2) {
+          index += index > curIndex ? -length : length; // Always go in the shortest direction
+        }
+        const newIndex = gsap.utils.wrap(0, length, index);
+        let time = times[newIndex]; 
+      
+        if (time > tl.time() !== index > curIndex) { // Handle timeline wrapping
+          vars.modifiers = { time: gsap.utils.wrap(0, tl.duration()) };
+          time += tl.duration() * (index > curIndex ? 1 : -1);
+        }
+        curIndex = newIndex;
+        vars.overwrite = true;
+        return tl.tweenTo(time, vars);
+      }
+
+    tl.next = vars => toIndex(curIndex + 1, vars);
+    tl.previous = vars => toIndex(curIndex - 1, vars);
+    tl.current = () => curIndex;
+    tl.toIndex = (index, vars) => toIndex(index, vars);
+    tl.times = times;
+    tl.progress(1, true).progress(0, true); // Pre-render for performance
+
+    return tl;
+  }
+
   return (
-    <ul
-      ref={tickerRef}
-      style={{
-        display: "flex",
-        overflow: "hidden",
-        whiteSpace: "nowrap",
-        padding: 0,
-        margin: 0,
-        background: "black",
-        color: "white",
-      }}
-    >
-      {/* These items will be animated */}
-      <li style={{ display: "inline-block", padding: "0 20px" }}>Test 1</li>
-      <li style={{ display: "inline-block", padding: "0 20px" }}>Test 2</li>
-      <li style={{ display: "inline-block", padding: "0 20px" }}>Test 3</li>
-      <li style={{ display: "inline-block", padding: "0 20px" }}>Test 4</li>
-      <li style={{ display: "inline-block", padding: "0 20px" }}>Test 5</li>
-    </ul>
+    <div ref={wrapperRef} className="wrapper" style={{ overflow: "hidden", whiteSpace: "nowrap", marginTop: "25px" }}>
+        
+      <div className="box" ref={el => (boxesRef.current[0] = el)} style={{ display: "inline-block", width: "100px", height: "100px" }}></div>
+      <div className="box" ref={el => (boxesRef.current[1] = el)} style={{ display: "inline-block", width: "100px", height: "100px" }}></div>
+      <div className="box" ref={el => (boxesRef.current[2] = el)} style={{ display: "inline-block", width: "100px", height: "100px" }}></div>
+      <div className="box" ref={el => (boxesRef.current[3] = el)} style={{ display: "inline-block", width: "100px", height: "100px" }}></div>
+      <div className="box" ref={el => (boxesRef.current[4] = el)} style={{ display: "inline-block", width: "100px", height: "100px" }}></div>
+      <div className="box" ref={el => (boxesRef.current[5] = el)} style={{ display: "inline-block", width: "100px", height: "100px" }}></div>
+      <div className="box" ref={el => (boxesRef.current[6] = el)} style={{ display: "inline-block", width: "100px", height: "100px" }}></div>
+      <div className="box" ref={el => (boxesRef.current[7] = el)} style={{ display: "inline-block", width: "100px", height: "100px" }}></div>
+      <div className="box" ref={el => (boxesRef.current[8] = el)} style={{ display: "inline-block", width: "100px", height: "100px" }}></div>
+      <div className="box" ref={el => (boxesRef.current[9] = el)} style={{ display: "inline-block", width: "100px", height: "100px" }}></div>
+    </div>
   );
 }
